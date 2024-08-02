991b9ba5355c451ed8f30a7aa902862a815bb8d6
mfrankl <mfrankl@seibert-media.net>
2024-08-02 13:03:07 +0200

    setup dotenv

--- src/App.ts
+++ src/App.ts
@@ -3,14 +3,18 @@ import { DataService } from "./DataService";
 import { ChatDAO } from "./ChatDao";
 import eventBus from "./EventBus";
 import { startRestServer } from "./rest/server";
+import dotenv from "dotenv";
+dotenv.config();
 
 function main() {
-  const botKey = "5333958735:AAHQ7LMmKvSbtWG4a0hkGbj_kDNBUid3_j0";
-  const botName = "KonvBot";
-  const redisHost = "";
+  const botKey = process.env.TELEGRAM_BOT_TOKEN;
+  const botName = process.env.BOT_NAME;
+  const redisHost = process.env.REDIS_HOST;
+  if (!botKey || !botName || redisHost == undefined) {
+    throw new Error("app secrets not defined.");
+  }
   const dataService = new DataService(new ChatDAO(redisHost));
-  const konvBot = new KonvBot(eventBus, botKey, botName, dataService);
-
+  new KonvBot(eventBus, botKey, botName, dataService);
   startRestServer();
 }
 
